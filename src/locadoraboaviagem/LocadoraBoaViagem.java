dbc2470525831e893859d5996f817be5fe0e4df8
Tiago Teixeira <Tiago Teixeira@TIAGO>
2020-09-23 21:08:45 -0300

    Feito Tela de Login

--- src/locadoraboaviagem/LocadoraBoaViagem.java
+++ src/locadoraboaviagem/LocadoraBoaViagem.java
@@ -7,7 +7,6 @@ public class LocadoraBoaViagem {
         //criando o objeto da tela login do sistema
         telas.JfrmLogin telalogin = new telas.JfrmLogin();
         //usando o metodo para mostrar a tela
-        
         telalogin.show();
          
     }
